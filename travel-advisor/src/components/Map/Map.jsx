4381a95ceed4517fcf12a7acdf1f58a0bb9df405
yuriLee <yuriuniv@gmail.com>
2022-01-18 00:01:00 +0900

    Feat: api의 getPlacesData 구성

--- travel-advisor/src/components/Map/Map.jsx
+++ travel-advisor/src/components/Map/Map.jsx
@@ -6,11 +6,9 @@ import Rating from "@material-ui/lab";
 
 import useStyles from "./styles";
 
-const Map = () => {
+const Map = ({ setCoordinates, setBounds, coordinates }) => {
   const classes = useStyles();
   const isMobile = useMediaQuery("(min-width:600px)");
-
-  const coordinates = { lat: 0, lng: 0 };
 
   return (
     <div className={classes.mapContainer}>
@@ -20,9 +18,12 @@ const Map = () => {
         center={coordinates}
         defaultZoom={14}
         margin={[50, 50, 50, 50]}
-        // options={""}
-        // onChange={""}
-        // onChildClick={""}
+        //options={""}
+        onChange={(e) => {
+          setCoordinates({ lat: e.center.lat, lng: e.center.lng });
+          setBounds({ ne: e.marginBounds.ne, sw: e.margin.sw });
+        }}
+        //onChildClick={""}
       ></GoogleMapReact>
     </div>
   );
