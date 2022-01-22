eac94fa155b3e771681afb5fb91744f957c4eca5
yuriLee <yuriuniv@gmail.com>
2022-01-22 20:55:50 +0900

    Feat: Type 필터 기능 구현 (Restaurants, Hotels, Attractions)

--- travel-advisor/src/App.js
+++ travel-advisor/src/App.js
@@ -14,6 +14,8 @@ const App = () => {
   const [bounds, setBounds] = useState({});
 
   const [isLoading, setIsLoading] = useState(false);
+  const [type, setType] = useState("restaurants");
+  const [rating, setRating] = useState("");
 
   useEffect(() => {
     navigator.geolocation.getCurrentPosition(
@@ -25,11 +27,11 @@ const App = () => {
 
   useEffect(() => {
     setIsLoading(true);
-    getPlacesData(bounds.sw, bounds.ne).then((data) => {
+    getPlacesData(type, bounds.sw, bounds.ne).then((data) => {
       setPlaces(data);
       setIsLoading(false);
     });
-  }, [coordinates, bounds]);
+  }, [type, coordinates, bounds]);
 
   return (
     <>
@@ -41,6 +43,10 @@ const App = () => {
             places={places}
             childClicked={childClicked}
             isLoading={isLoading}
+            type={type}
+            setType={setType}
+            rating={rating}
+            setRating={setRating}
           />
         </Grid>
         <Grid item xs={12} md={8}>
--- travel-advisor/src/api/index.js
+++ travel-advisor/src/api/index.js
@@ -1,25 +1,26 @@
 import axios from "axios";
 
-const URL =
-  "https://travel-advisor.p.rapidapi.com/restaurants/list-in-boundary";
-
-export const getPlacesData = async (sw, ne) => {
+export const getPlacesData = async (type, sw, ne) => {
   try {
     const {
       data: { data },
-    } = await axios.get(URL, {
-      params: {
-        bl_latitude: sw.lat,
-        tr_latitude: ne.lat,
-        bl_longitude: sw.lng,
-        tr_longitude: ne.lng,
-        // lang: "ko_KR",
-      },
-      headers: {
-        "x-rapidapi-key": "d023956d74msh754dc11ee0f3073p1dc251jsna5019d9e0876",
-        "x-rapidapi-host": "travel-advisor.p.rapidapi.com",
-      },
-    });
+    } = await axios.get(
+      `https://travel-advisor.p.rapidapi.com/${type}/list-in-boundary`,
+      {
+        params: {
+          bl_latitude: sw.lat,
+          tr_latitude: ne.lat,
+          bl_longitude: sw.lng,
+          tr_longitude: ne.lng,
+          // lang: "ko_KR",
+        },
+        headers: {
+          "x-rapidapi-key":
+            "d023956d74msh754dc11ee0f3073p1dc251jsna5019d9e0876",
+          "x-rapidapi-host": "travel-advisor.p.rapidapi.com",
+        },
+      }
+    );
 
     return data;
   } catch (error) {
